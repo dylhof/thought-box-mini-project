bbc6b4319adb0207591b768806e58be11d1917f6
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-20 16:36:30 -0700

    Add functions and tests to thoughtCard

--- src/thoughtCard/thoughtCard.js
+++ src/thoughtCard/thoughtCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export const ThoughtCard = () => {
+export const ThoughtCard = (props) => {
   return (
-    <div>
-      ThoughtCard
+    <div className='card-div' id={props.thought.id}>
+      <h3>{props.thought.title}</h3>
+      <p>{props.thought.body}</p>
+      <button className='card-delete-btn' onClick={() => {props.deleteThought(props.thought.id)}}>Remove Thought</button>
     </div>
   )
 }
--- src/thoughtCard/thoughtCard.test.js
+++ src/thoughtCard/thoughtCard.test.js
@@ -3,9 +3,26 @@ import { shallow } from 'enzyme';
 
 import { ThoughtCard } from './ThoughtCard.js';
 
-describe('ThoughtCard', () => {
+const mockThought = { title: 'Hello', body: 'World', id: 0 };
+const deleteThoughtMock = jest.fn();
 
+describe('ThoughtCard', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <ThoughtCard 
+        thought={mockThought}
+        deleteThought={deleteThoughtMock}
+      />
+    )
+  })
   it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
   });
+
+  it('should call deleteThought when remove button is clicked', () => {
+    wrapper.find('.card-delete-btn').simulate('click');
+    expect(deleteThoughtMock).toBeCalledWith(0)
+  })
 
 });
