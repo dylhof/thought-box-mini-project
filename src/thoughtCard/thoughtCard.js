5327cfff4c8e6984ae440bd3c3fbee8a772817d1
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-21 15:47:03 -0700

    Change delete button to be a trashcan symbol

--- src/thoughtCard/thoughtCard.js
+++ src/thoughtCard/thoughtCard.js
@@ -5,7 +5,7 @@ export const ThoughtCard = (props) => {
     <div className='card-div' id={props.thought.id}>
       <h3>{props.thought.title}</h3>
       <p>{props.thought.body}</p>
-      <button className='card-delete-btn' onClick={() => {props.deleteThought(props.thought.id)}}>Remove Thought</button>
+      <i className='card-delete-btn' onClick={() => {props.deleteThought(props.thought.id)}}class="fas fa-trash-alt"></i>
     </div>
   )
 }
