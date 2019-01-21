e06b49cb76721f524d57caaa7ecb05fb059e5fa8
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-21 08:34:10 -0700

    Create styles files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/main.scss';
 import CreateThought from './createThought/createThought';
 import { ThoughtList } from './thoughtList/thoughtList';
 
